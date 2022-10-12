5d84b39b2f4b7fb6d79377fa6635a4a8812eee0b
rafaelmdsouza <rafael.moraes.ds97@gmail.com>
2022-10-12 15:25:42 -0300

    final html changes

--- src/app/list-component/list-component.component.ts
+++ src/app/list-component/list-component.component.ts
@@ -1,10 +1,9 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PersonViewModel } from '../model/PersonViewModel';
 import { ListComponentService } from './service/list-component.service';
 import { MatDialog } from '@angular/material/dialog';
 import { AddPersonComponent } from './add-person/add-person.component';
-import { MatSort, Sort } from '@angular/material/sort';
-import { debounceTime } from 'rxjs';
+import { MatSort, MatSortable, Sort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 
@@ -18,8 +17,6 @@ export class ListComponentComponent implements OnInit {
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatTable) table: MatTable<any>;
 
-  sortSetting: Sort
-
   displayedColumns: string[] = [
     'name',
     'gender',
@@ -28,22 +25,23 @@ export class ListComponentComponent implements OnInit {
     'neighborhood',
     'action',
   ];
-  dataSource: MatTableDataSource<any>
+
+  dataSource: MatTableDataSource<any>;
   isPersonPosted: boolean = false;
 
   constructor(public service: ListComponentService, public dialog: MatDialog) {}
 
   ngOnInit() {
     this.getPerson();
-    this.sortSetting = {active: 'name', direction:'asc'}
   }
 
   getPerson() {
     this.service.getPerson().subscribe((response) => {
       this.dataSource = new MatTableDataSource(response);
       this.dataSource.paginator = this.paginator;
+      this.sort.sort({ id: 'name', start: 'asc' } as MatSortable);
       this.dataSource.sort = this.sort;
-      this.sortData(this.sortSetting);
+      this.sortData(this.dataSource.sort);
     });
   }
 
@@ -53,11 +51,14 @@ export class ListComponentComponent implements OnInit {
   }
 
   openDialog() {
-    this.dialog.open(AddPersonComponent, {
+    this.dialog
+      .open(AddPersonComponent, {
         minWidth: '550px',
-      }).afterClosed().subscribe(res => {
-        this.getPerson()
       })
+      .afterClosed()
+      .subscribe((res) => {
+        this.getPerson();
+      });
   }
 
   deletePerson(id: number) {
@@ -66,21 +67,34 @@ export class ListComponentComponent implements OnInit {
     });
   }
 
-  sortData(dataSort: Sort){
-    this.sortSetting = dataSort;
-
-    if(!dataSort.active || dataSort.direction =='')
-    return
-
-    this.dataSource.sortData = (data: PersonViewModel[], sort: MatSort) => {
-      return data.sort((a, b) => {
-        let directionAsc = sort.direction === 'asc';
-          return this.compare(a.name.toLowerCase(), b.name.toLowerCase(), directionAsc)
-      });
-     }
+  sortData(dataSort: Sort) {
+    if (!dataSort.active || dataSort.direction == '') {
+      this.dataSource.sort = this.sort;
+      return;
+    }
+      this.verifyActiveSort(dataSort.active)
   }
 
-  private compare(a: number|string, b: number | string, isAsc: boolean) {
+  private verifyActiveSort(sortActive: string){
+    if (sortActive === 'name') {
+      this.dataSource.sortData = (data: PersonViewModel[], sort: MatSort) => {
+        return data.sort((a, b) => {
+          let directionAsc = sort.direction === 'asc';
+          return this.compare(a.name.toLowerCase(),b.name.toLowerCase(), directionAsc);
+        });
+      };
+    }
+    if (sortActive === 'gender') {
+      this.dataSource.sortData = (data: PersonViewModel[], sort: MatSort) => {
+        return data.sort((a, b) => {
+          let directionAsc = sort.direction === 'asc';
+          return this.compare(a.gender, b.gender, directionAsc);
+        });
+      };
+    }
+  }
+
+  private compare(a: number | string, b: number | string, isAsc: boolean) {
     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
   }
 }
